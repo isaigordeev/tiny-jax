7b4332d155b40e8e4fd880f6e940b4b19db4be71
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2023-08-12 23:43:49 +0200

    add example without backpass

--- node.py
+++ node.py
@@ -1,6 +1,14 @@
 class Node:
-    def __init__(self, value=0, _previous_node=()):
+    def __init__(self, value=0, _previous_nodes=(), _operator=None):
         self.value = value
         self.grad = 0
-        self._previous_node = set(_previous_node)
+        self._previous_node = set(_previous_nodes)
         self._backward_function = lambda: None
+
+
+    def __add__(self, other):
+        other = other if isinstance(other, Node) else Node(other)
+        out = Node(self.value + other.value, (self, other), '+')
+
+
+        return out
