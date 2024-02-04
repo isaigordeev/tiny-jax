81ed7f2a45684955310707182212dec4791e4717
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2024-02-04 23:33:49 +0100

    little setup

---
+++ _C/src.cpp
@@ -0,0 +1,31 @@
+// hello.cpp
+#include <Python.h>
+
+// Simple "Hello, World!" function
+static PyObject* hello_world(PyObject* self, PyObject* args) {
+    // Print "Hello, World!" to the console
+    printf("Hello, World!\n");
+
+    // Return None
+    Py_RETURN_NONE;
+}
+
+// Method mapping for the module
+static PyMethodDef methods[] = {
+    {"hello_world", hello_world, METH_NOARGS, "Print 'Hello, World!'"},
+    {NULL, NULL, 0, NULL}  // Sentinel
+};
+
+// Module definition
+static struct PyModuleDef module = {
+    PyModuleDef_HEAD_INIT,
+    "hello",     // Module name
+    NULL,        // Module documentation
+    -1,          // Size of per-interpreter state, or -1 if the module keeps state in global variables.
+    methods      // Method mapping
+};
+
+// Module initialization function
+PyMODINIT_FUNC PyInit_hello(void) {
+    return PyModule_Create(&module);
+}
